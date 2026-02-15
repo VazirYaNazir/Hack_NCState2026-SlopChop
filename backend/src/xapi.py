f1d7cd519c30bffbe55907fa4da4dbbafaa1141c
Parsa <parsanemati62@gmail.com>
2026-02-15 03:45:41 -0300

    all

--- backend/src/xapi.py
+++ backend/src/xapi.py
@@ -1,5 +1,6 @@
 import tweepy as tw
 from dotenv import load_dotenv
+import json
 import os
 
 load_dotenv()
@@ -9,3 +10,7 @@ client = tw.Client(bearer_token=xapi)
 # We need to first get the location of the user,
 # using latitude and longitude, coordinates.
 
+lat_state = 35.7833592
+long_state = -78.670738
+
+trends = client.closest_trends(lat=lat_state, long=long_state)
